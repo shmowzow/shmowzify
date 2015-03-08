fc843aa5305c14200191953a6f88cc06def6232b
Matheus Brasil <matheus.brasil10@gmail.com>
2015-03-08 15:54:41 -0300

    Add brainfuck2shmowzow converter

---
+++ brainfuck2shmowzow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+# What happens when you compile Brainfuck to Shmowzow:
+
+# ">" becomes "Shmowzow"
+# "<" becomes "shmowzoW"
+# "+" becomes "SHMOWZOW"
+# "-" becomes "shmowzow"
+# "." becomes "ShMoWzOw"
+# "," becomes "sHmOwZoW"
+# "[" becomes "ShmoWzow"
+# "]" becomes "sHMOwZOW"
+ 
+sed "s/>/Shmowzow\n/g ; s/</shmowzoW\n/g ; s/+/SHMOWZOW\n/g ; s/-/shmowzow\n/g ; s/\./ShMoWzOw\n/g ; s/,/sHmOwZoW\n/g ; s/\[/ShmoWzow\n/g ; s/\]/sHMOwZOW\n/g" | grep -v '^$'
